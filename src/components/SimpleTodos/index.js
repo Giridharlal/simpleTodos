834df5fd486cd84ee55b036c87feb2eb5ae317c5
Giridharlal <giridhar99lal@gmail.com>
2025-03-03 06:22:55 +0000

    update

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,3 +1,8 @@
+// Write your code here
+import React, {useState} from 'react'
+import TodoItem from '../TodoItem'
+import './index.css'
+
 const initialTodosList = [
   {
     id: 1,
@@ -31,31 +36,91 @@ const initialTodosList = [
     id: 8,
     title: 'Get essentials for Sunday car wash',
   },
+  {
+    id: 9,
+    title: 'Buy groceries',
+  },
 ]
 
-// Write your code here
-import React, { useState } from 'react';
-import TodoItem from '../TodoItem';
-import './index.css';
-
 const SimpleTodos = () => {
-  const [todos, setTodos] = useState(initialTodosList);
+  const [todos, setTodos] = useState(initialTodosList)
+  const [newTitle, setNewTitle] = useState('')
+  const [isAdding, setIsAdding] = useState(false)
+  console.log(todos)
 
-  const deleteTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
-  };
+  const deleteTodo = id => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos(updatedTodos)
+  }
+
+  const saveTodo = (id, value) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? {...todo, title: value, completed: false} : todo,
+    )
+    setTodos(updatedTodos)
+  }
+
+  const addTodo = title => {
+    const exNumber = title.split(' ')
+    const num = exNumber.at(-1)
+    console.log(exNumber.at(-1))
+    if (isNaN(Number(num))) {
+      const newTodo = {id: todos.length + 1, title, completed: false}
+      setTodos(prevTodos => [...prevTodos, newTodo])
+    } else {
+      const text = title.slice(0, -num.length)
+      console.log(text, 'text')
+      console.log('exNumber.length:', -num.length)
+      setTodos(prevTodos => {
+        const newTodos = Array.from({length: num}, (_, i) => ({
+          id: prevTodos.length + i + 1,
+          title: text,
+          completed: false,
+        }))
+        return [...prevTodos, ...newTodos]
+      })
+    }
+  }
+
+  const toggleCompletion = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id
+          ? {...todo, completed: todo.completed ? !todo.completed : true}
+          : todo,
+      ),
+    )
+  }
 
   return (
     <div className="simple-todos-container">
       <h1 className="heading">Simple Todos</h1>
       <ul className="todos-list">
         {todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            saveTodo={saveTodo}
+            toggleCompletion={toggleCompletion}
+          />
         ))}
       </ul>
+      <input
+        type="textbox"
+        value={newTitle}
+        onChange={e => setNewTitle(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          addTodo(newTitle)
+          setIsAdding(!isAdding)
+        }}
+      >
+        Add
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default SimpleTodos;
+export default SimpleTodos
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,16 +1,54 @@
 // Write your code here
-import React from 'react';
-import './index.css';
+import React, {useState} from 'react'
+import './index.css'
 
-const TodoItem = ({ todo, deleteTodo }) => {
-  const { id, title } = todo;
-
+const TodoItem = ({todo, deleteTodo, saveTodo, toggleCompletion}) => {
+  const {id, title, completed} = todo
+  const [newValue, setNewValue] = useState(title)
+  const [isEditing, setIsEditing] = useState(false)
+  console.log(completed)
   return (
     <li className="todo-item">
-      <p className="todo-title">{title}</p>
-      <button className="delete-button" onClick={() => deleteTodo(id)}>Delete</button>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => toggleCompletion(id)}
+      />
+      {!isEditing ? (
+        <p className={completed ? 'strike' : 'todo-title'}>{title}</p>
+      ) : (
+        ''
+      )}
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={newValue}
+            onChange={e => setNewValue(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              saveTodo(id, newValue)
+              setIsEditing(!isEditing)
+            }}
+          >
+            save
+          </button>
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            setIsEditing(!isEditing)
+          }}
+        >
+          edit
+        </button>
+      )}
+      <button className="delete-button" onClick={() => deleteTodo(id)}>
+        Delete
+      </button>
     </li>
-  );
-};
+  )
+}
 
-export default TodoItem;
+export default TodoItem
